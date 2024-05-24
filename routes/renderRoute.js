bfa37666774018b16eac7b5e4b87690cdd79091c
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-05-25 03:39:16 +0500

    added login, register layout

--- routes/renderRoute.js
+++ routes/renderRoute.js
@@ -6,12 +6,24 @@ router.use((req, res, next) => {
     next();
 });
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     res.render('index', {pageName: 'index'});
 })
 
-router.get('/product', async (req, res) => {
+router.get('/product', (req, res) => {
     res.render('product', {pageName: 'product'});
+})
+
+router.get('/profile', (req, res) => {
+    res.render('profile', {pageName: 'profile'});
+})
+
+router.get('/auth/login', (req, res) => {
+    res.render('auth/login', {pageName: 'login'});
+})
+
+router.get('/auth/register', (req, res) => {
+    res.render('auth/register', {pageName: 'register'});
 })
 
 module.exports = router
