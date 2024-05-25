2281ded595ef1640f5356866235e2b19f9373838
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-05-25 08:50:42 +0500

    added admin pages

--- routes/renderRoute.js
+++ routes/renderRoute.js
@@ -7,27 +7,39 @@ router.use((req, res, next) => {
 });
 
 router.get('/', (req, res) => {
-    res.render('index', {pageName: 'index'});
+    res.render('index', {pageName: 'index', role: "user"});
 })
 
 router.get('/product', (req, res) => {
-    res.render('product', {pageName: 'product'});
+    res.render('product', {pageName: 'product', role: "user"});
 })
 
 router.get('/profile', (req, res) => {
-    res.render('profile/profile', {pageName: 'profile'});
+    res.render('profile/profile', {pageName: 'profile', role: "user"});
 })
 
 router.get('/profile-confirm', (req, res) => {
-    res.render('profile/profile-confirm', {pageName: 'profile'});
+    res.render('profile/profile-confirm', {pageName: 'profile', role: "user"});
 })
 
 router.get('/auth/login', (req, res) => {
-    res.render('auth/login', {pageName: 'login'});
+    res.render('auth/login', {pageName: 'login', role: "user"});
 })
 
 router.get('/auth/register', (req, res) => {
-    res.render('auth/register', {pageName: 'register'});
+    res.render('auth/register', {pageName: 'register', role: "user"});
+})
+
+router.get('/admin/users', (req, res) => {
+    res.render('admin/admin-users', {pageName: 'admin-users', role: "admin"});
+})
+
+router.get('/admin/products', (req, res) => {
+    res.render('admin/admin-products', {pageName: 'admin-products', role: "admin"});
+})
+
+router.get('/admin/categories', (req, res) => {
+    res.render('admin/admin-categories', {pageName: 'admin-categories', role: "admin"});
 })
 
 module.exports = router
